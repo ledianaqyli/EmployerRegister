d2f36c9d1afab2a5328acec8c35620c7e8d6e71e
Lediana <ledianaqyli@gmail.com>
2023-08-10 17:27:01 +0200

    Creating Add funcionality and the View

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,44 @@
+﻿using EmployerRegister.Data;
+using EmployerRegister.Models;
+using EmployerRegister.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployerRegister.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MVCDemoDbContext context;
+        public EmployeesController(MVCDemoDbContext context)
+        {
+            this.context = context;
+
+
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task< IActionResult> Add(AddEmployeViewModel addEmploye)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmploye.Name,
+                Email = addEmploye.Email,
+                Salary = addEmploye.Salary,
+                DateofBirth = addEmploye.DateofBirth,
+                Department = addEmploye.Department,
+
+            };
+
+           await context.Employees.AddAsync(employee);
+           await context.SaveChangesAsync();
+            return RedirectToAction("Add");
+
+        }
+    }
+}
---
+++ Models/AddEmployeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployerRegister.Models
+{
+    public class AddEmployeViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public string Department { get; set; }
+    }
+}
