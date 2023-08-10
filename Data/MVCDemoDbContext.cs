fd81b45ed91e501f81b768ec0527b3c5bf0ab30a
Lediana <ledianaqyli@gmail.com>
2023-08-10 16:25:23 +0200

    Add project files.

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployerRegister.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployerRegister.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options ) : base(options) { 
+        
+        }
+
+        public DbSet<Employee> Employees{ get; set; }
+
+    }
+}
